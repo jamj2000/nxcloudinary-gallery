d950433b7871824309342548bcfccae7ac60618c
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-12-07 21:51:04 +0100

    Corregido .env

---
+++ src/app/galeria.js
@@ -0,0 +1,20 @@
+
+function Galeria({result}) {
+//   console.log(result);
+  return (
+    <>
+    <h1>Galería de imágenes</h1>
+     {
+        result.resources
+          .filter ( r => r.format !== 'json' )
+          .map( r => (
+              <img key={r.public_id} src={r.secure_url} 
+                   style={{ aspectRatio: r.width / r.height, height: '300px' }}
+              />
+          ))
+     }
+    </>
+  )
+}
+
+export default Galeria

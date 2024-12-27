6306dd70cd52a82fbfc5313539680fc8ac27b864
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-12-27 22:54:12 +0100

    ...

--- src/app/galeria.js
+++ src/app/galeria.js
@@ -1,11 +1,11 @@
-import {v2 as cloudinary} from 'cloudinary';
-import process from 'node:process'
+import { v2 as cloudinary } from 'cloudinary';
+// import process from 'node:process'
 
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 async function Galeria() {
@@ -13,20 +13,23 @@ async function Galeria() {
   const result = await cloudinary.api.resources({
     max_results: 500,
     type: 'upload',
-    prefix: 'samples/landscapes'
+    // prefix: 'samples/landscapes'
+    prefix: 'tienda/productos'
   });
   // console.log(result);
+
+
   return (
     <>
-    <h1>Galería de imágenes</h1>
-     {
+      <h1>Galería de imágenes</h1>
+      {
         result.resources
-          .map( r => (
-              <img key={r.public_id} src={r.secure_url} 
-                   style={{ aspectRatio: r.width / r.height, height: '300px' }}
-              />
+          .map(r => (
+            <img key={r.public_id} src={r.secure_url}
+              style={{ aspectRatio: r.width / r.height, height: '300px' }}
+            />
           ))
-     }
+      }
     </>
   )
 }

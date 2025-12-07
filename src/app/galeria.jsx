263def7ef7169a6a6c9e54b2c2d5e990e4cec9b6
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-12-07 22:50:35 +0100

    CLOUDINARY_URL

--- src/app/galeria.jsx
+++ src/app/galeria.jsx
@@ -1,12 +1,13 @@
 import { v2 as cloudinary } from 'cloudinary';
 // import process from 'node:process'
 
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+cloudinary.config(process.env.CLOUDINARY_URL || '')
 
 async function Galeria() {
 
